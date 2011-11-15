3aa3b785aec9e7705495cafd6fac346cbf25cb3c
Tuan Nguyen <nguyen.tt.1978@gmail.com>
2011-11-16 00:36:22 +0100

    Enabled Radiant

--- config/environment.rb
+++ config/environment.rb
@@ -1,41 +1,85 @@
 # Be sure to restart your server when you modify this file
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+# Uncomment below to force Rails into production mode when
+# you don't control web/app server and can't set it the proper way
+# ENV['RAILS_ENV'] ||= 'production'
 
-# Bootstrap the Rails environment, frameworks, and default configuration
+# Specifies gem version of Rails to use when vendor/rails is not present
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
+require 'radius'
 
-  # Add additional load paths for your own custom dirs
-  # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
+Radiant::Initializer.run do |config|
+  # Skip frameworks you're not going to use (only works if using vendor/rails).
+  # To use Rails without a database, you must remove the Active Record framework
+  config.frameworks -= [ :action_mailer ]
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  # Only load the extensions named here, in the order given. By default all
+  # extensions in vendor/extensions are loaded, in alphabetical order. :all
+  # can be used as a placeholder for all extensions not explicitly named.
+  # config.extensions = [ :all ]
+  
+  # By default, only English translations are loaded. Remove any of these from
+  # the list below if you'd like to provide any of the supported languages
+  config.extensions -= [:dutch_language_pack, :french_language_pack, :german_language_pack,
+                        :italian_language_pack, :japanese_language_pack, :russian_language_pack]
 
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  # Your secret key for verifying cookie session data integrity.
+  # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random,
+  # no regular words or you'll be exposed to dictionary attacks.
+  config.action_controller.session = {
+    :key => '_i-savant.com_session',
+    :secret      => '872e749c353e45ca4a805d7681402fc82ba81594'
+  }
 
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  # Comment out this line if you want to turn off all caching, or
+  # add options to modify the behavior. In the majority of deployment
+  # scenarios it is desirable to leave Radiant's cache enabled and in
+  # the default configuration.
+  #
+  # Additional options:
+  #  :use_x_sendfile => true
+  #    Turns on X-Sendfile support for Apache with mod_xsendfile or lighttpd.
+  #  :use_x_accel_redirect => '/some/virtual/path'
+  #    Turns on X-Accel-Redirect support for nginx. You have to provide
+  #    a path that corresponds to a virtual location in your webserver
+  #    configuration.
+  #  :entitystore => "radiant:tmp/cache/entity"
+  #    Sets the entity store type (preceding the colon) and storage
+  #   location (following the colon, relative to Rails.root).
+  #    We recommend you use radiant: since this will enable manual expiration.
+  #  :metastore => "radiant:tmp/cache/meta"
+  #    Sets the meta store type and storage location.  We recommend you use
+  #    radiant: since this will enable manual expiration and acceleration headers.
+  config.middleware.use ::Radiant::Cache
+
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
+  # (create the session table with 'rake db:sessions:create')
+  config.action_controller.session_store = :cookie_store
 
   # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  config.active_record.observers = :user_action_observer
 
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
+  # Make Active Record use UTC-base instead of local time
   config.time_zone = 'UTC'
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
+  # Set the default field error proc
+  config.action_view.field_error_proc = Proc.new do |html, instance|
+    if html !~ /label/
+      %{<span class="error-with-field">#{html} <span class="error">#{[instance.error_message].flatten.first}</span></span>}
+    else
+      html
+    end
+  end
+
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+
+  config.after_initialize do
+    # Add new inflection rules using the following format:
+    ActiveSupport::Inflector.inflections do |inflect|
+      inflect.uncountable 'config'
+    end
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,23 +6,33 @@
 # and recreated between test runs.  Don't rely on the data there!
 config.cache_classes = true
 
+# ensure test extensions are loaded
+# test_extension_dir = File.join(File.expand_path(RADIANT_ROOT), 'test', 'fixtures', 'extensions')
+# config.extension_paths.unshift test_extension_dir
+# config.extension_paths.uniq!
+# if !config.extensions.include?(:all)
+#   config.extensions.concat(Dir["#{test_extension_dir}/*"].sort.map {|x| File.basename(x).sub(/^\d+_/,'')})
+#   config.extensions.uniq!
+# end
+
 # Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+config.whiny_nils    = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
-config.action_view.cache_template_loading            = true
 
 # Disable request forgery protection in test environment
 config.action_controller.allow_forgery_protection    = false
 
-# Tell Action Mailer not to deliver emails to the real world.
+# Tell ActionMailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-# Use SQL instead of Active Record's schema dumper when creating the test database.
-# This is necessary if your schema can't be completely dumped by the schema dumper,
-# like if you have constraints or database-specific column types
-# config.active_record.schema_format = :sql
+config.gem 'cucumber-rails',   :lib => false, :version => '>=0.2.4' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber-rails'))
+config.gem 'database_cleaner', :lib => false, :version => '>=0.4.3' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
+config.gem 'webrat',           :lib => false, :version => '>=0.6.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
+config.gem 'rspec',            :lib => false, :version => '>=1.3.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+
